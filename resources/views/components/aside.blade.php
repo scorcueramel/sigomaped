90b9b431162e17e9d27b13fcfe41022a852a9fa1
Alvaro Mauricio Acosta Salas <alvaro.acosta@munsurco.gob.pe>
2024-10-16 16:22:29 -0500

    16102024 - program crud v2

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -197,21 +197,16 @@ $usuario = \App\Models\User::find(Auth::id())->with('persona')->get()[0];
             <li class="header nav-small-cap">PROGRAMAS</li>
             <li
                 class="treeview {{ $activePage == 'programas.index' || $activePage == 'programas.create' ? 'menu-open' : '' }}">
-                <a href="#">
+                <a href="#" onclick="">
                     <i class="fa fa-edit"></i> <span>Programas</span>
                     <span class="pull-right-container">
                         <i class="fa fa-angle-right pull-right"></i>
                     </span>
                 </a>
-
-            </li>
                 <ul class="treeview-menu {{ $activePage == 'programas.index' || $activePage == 'programas.create' ? 'd-block' : '' }}"
                     id="programas">
                     <li class="{{ $activePage == 'programas.index' ? 'active' : '' }}"><a
                             href="{{ route('programas.index') }}"><i class="fa fa-circle-thin"></i>Listado</a>
-                    </li>
-                    <li class="{{ $activePage == 'programas.create' ? 'active' : '' }}"><a
-                            href="{{ route('programas.create') }}"><i class="fa fa-circle-thin"></i>Nuevo Programa</a>
                     </li>
                 </ul>
             </li>
