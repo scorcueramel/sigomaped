70b7401bbb97208c148fd926940ef09483e3f65c
scorcueramel <46231549+scorcueramel@users.noreply.github.com>
2024-10-21 11:36:53 -0500

    21102024

--- database/migrations/2024_09_27_212149_create_periodos_table.php
+++ database/migrations/2024_09_27_212149_create_periodos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('periodos', function (Blueprint $table) {
             $table->id();
-            $table->string('periodo',1);
+            $table->string('periodo',5);
             $table->timestamps();
         });
     }
